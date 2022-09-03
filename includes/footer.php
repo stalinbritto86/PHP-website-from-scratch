1c885def1073baeabc2e1860b7b53258b4c2f8cb
stalinbritto86 <stalinbritto86@gmail.com>
2022-09-03 21:59:15 +0530

    3rd day of the project

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,27 @@
  */
 ?>
 
-<footer><div id="footer"><p class="copyright-text">Copyrights © 2022 My Brand. All Rights Reserved.</p></div></footer>
+<footer><div id="footer">
+<svg class="footer-wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 100" preserveAspectRatio="none">
+        <path class="footer-wave-path" d="M851.8,100c125,0,288.3-45,348.2-64V0H0v44c3.7-1,7.3-1.9,11-2.9C80.7,22,151.7,10.8,223.5,6.3C276.7,2.9,330,4,383,9.8 c52.2,5.7,103.3,16.2,153.4,32.8C623.9,71.3,726.8,100,851.8,100z"></path>
+      </svg>
+
+<div id="section-1"><div class="section-1-cont">
+<div class="subsection-1"><i class="ri-map-pin-fill"></i>Find us<span>12/2-7D, Abcde, Dedddfgdgff.</span></div>
+<div class="subsection-2"><i class="ri-phone-fill"></i>Call us<span>+91-1234568790</span></div>
+<div class="subsection-3"><i class="ri-mail-fill"></i>Mail us<span>dummymail@gmail.com</span></div>
+</div></div>
+
+<div id="section-2"><div class="section-1-cont">
+<div class="subsection-1">MY BRAND</div>
+<div class="subsection-2">LINKS</div>
+<div class="subsection-3">HEAD OFFICE</div>
+<div class="subsection-4">SOCIAL</div>
+</div></div>
+
+<div id="section-3"><p class="copyright-text">Copyrights © 2022 My Brand. All Rights Reserved.</p></div>
+
+</div></footer>
 
 </body>
 
